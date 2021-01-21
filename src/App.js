f713191edee495e2da8f305e48073e0f2ed576f0
jboe10 <jboesur@gmail.com>
2021-01-20 21:16:44 -0800

    cleaned up and reorganzied code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './styles/main.sass';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
-
-import Work from './components/work/Work';
-import Info from './components/info/Info';
-import Store from './components/store/Store';
+// import Work from './components/work/Work';
+// import Info from './components/info/Info';
+// import Store from './components/store/Store';
 import Contact from './components/contact/Contact';
-import Cultish from './components/cultish/Cultish';
-import Header from './components/Header';
-import Footer from './components/Footer';
+// import Cultish from './components/cultish/Cultish';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
 
 import './App.css';
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-import './Header.scss'
-
-import Work from './work/Work';
-import Info from './info/Info';
-import Store from './store/Store';
-import Contact from './contact/Contact';
+import {Link} from "react-router-dom";
+// import Work from './work/Work';
+// import Info from './info/Info';
+// import Store from './store/Store';
+// import Contact from './contact/Contact';
 
 export default class Header extends Component {
-
 	constructor(props) {
 		super(props);
 		var date = new Date(); /* creating object of Date class */
-		console.log(date.getTimezoneOffset());
 		this.state = {
 			currentPath:window.location.pathname,
    		hour : date.getHours(),
@@ -28,9 +18,11 @@ export default class Header extends Component {
 	}
 
 	currentTime() {
-		 this.state.hour = this.updateTime(this.state.hour);
-		 this.state.min = this.updateTime(this.state.min);
-		 this.state.sec = this.updateTime(this.state.sec);
+		 this.setState({
+			hour: this.updateTime(this.state.hour),
+			min: this.updateTime(this.state.min),
+			sec: this.updateTime(this.state.sec)
+		 })
 	}
 
 	updateTime(k) {
@@ -42,26 +34,24 @@ export default class Header extends Component {
 		}
 	}
 
-	
 	render() {
 		return (
-			<div className="header">
-				<a name="top"></a>
+			<div className="header" id="top">
 				<ul className="header-links">
 					<li>
 						<Link to="/" className={(this.props.to === this.state.currentPath)?"active-page":"inactive-page"}>Cultish R</Link>
 					</li>
 					<li>  
-						<Link to="#" className={(this.props.to ===  this.state.currentPath)?"active-page":"inactive-page"}>Work</Link>
+						<Link to="/" className={(this.props.to ===  this.state.currentPath)?"active-page":"inactive-page"}>Work</Link>
 					</li>
 					<li>
-						<Link to="#" className={(this.props.to ===  this.state.currentPath)?"active-page":"inactive-page"}>Info</Link>
+						<Link to="/" className={(this.props.to ===  this.state.currentPath)?"active-page":"inactive-page"}>Info</Link>
 					</li>
 					<li>
-						<Link to="#" className={(this.props.to ===  this.state.currentPath)?"active-page":"inactive-page"}>Store</Link>
+						<Link to="/" className={(this.props.to ===  this.state.currentPath)?"active-page":"inactive-page"}>Store</Link>
 					</li>  
 					<li>
-						<Link to="#" className={(this.props.to ===  this.state.currentPath)?"active-page":"inactive-page"}>Contact</Link>
+						<Link to="/" className={(this.props.to ===  this.state.currentPath)?"active-page":"inactive-page"}>Contact</Link>
 					</li>
 				</ul>
 				<div className="header-info">
